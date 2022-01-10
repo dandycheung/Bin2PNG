033cf8ff7b8206d775ffd7a91b9402f2483026bf
Dandy Cheung <dandycheung@hotmail.com>
2022-01-10 15:24:54 +0800

    输入输出文件参数的支持达到基本可用。

--- bin2png.c
+++ bin2png.c
@@ -2,8 +2,15 @@
 #include <stdlib.h>
 #include <math.h>
 
+#include <sys/stat.h> // the only stuff that break the cross-platform capability
+
 #include "cwalk.h"
 #include "lodepng.h"
+
+#ifndef MAX_PATH
+#define MAX_PATH 256
+#define MAX_PATHx2 512
+#endif
 
 #define PNG_FILE "image.png"
 #define BIN_IN_FILE "in.exe"
@@ -13,9 +20,16 @@ int printUsage(int argc, char *argv[]);
 int binaryToPng(char *infile, char *outfile);
 int pngToBinary(char *infile, char *outfile);
 
+bool isDir(const char *path);
+
 void main(int argc, char *argv[]) {
 	char *infile, *outfile;
 	int mode = -1;
+
+	bool flag;
+	char cwd[MAX_PATH];
+	char ifnbuff[MAX_PATHx2];
+	char ofnbuff[MAX_PATHx2];
 
 	if (argc < 3 || argc > 4) {
 		printUsage(argc, argv);
@@ -32,15 +46,52 @@ void main(int argc, char *argv[]) {
 		return;
 	}
 
-	// TODO:
-	// 此处需要根据输入参数，整理出输入文件和输出文件的绝对路径。规则如下。
-	// 1. 如果参数完整，首先要将输入文件名和输出文件名绝对路径化（如果输入参数为相对路径，则要引入当前工作目录进行拼装，然后绝对化）；
-	// 1.1 如果输入路径不是合法文件，或者改文件不存在，则报错退出；
-	// 1.2 如果输出路径中没有文件名部分（也即以 / 结尾），则文件名根据输入文件名生成；如果输入文件名具有扩展名，则将该扩展名变更为 png，否则追加 png 扩展名；
-	// 2. 如果参数少一个，则认为缺少了输出文件名，将根据 mode 和输入文件名进行生成；路径保持与输入文件相同，文件名生成规则与 1.2 中的规则相同。
+	// 需要根据整理出输入文件和输出文件的绝对路径 -->
+	// 1. 将输入文件名绝对路径化（如果输入参数为相对路径，则要引入当前工作目录进行拼装，然后绝对化）；
+	flag = cwk_path_is_absolute(argv[2]);
+	if (flag)
+		cwk_path_normalize(argv[2], ifnbuff, MAX_PATHx2);
+	else {
+		getcwd(cwd, MAX_PATH);
+		cwk_path_join(cwd, argv[2], ifnbuff, MAX_PATHx2);
+	}
 
-	infile = mode == 0 ? BIN_IN_FILE : PNG_FILE;
-	outfile = mode == 0 ? PNG_FILE : BIN_OUT_FILE;
+	printf("input file name is %s, a %s path; processed to %s.\n", argv[2], flag ? "absolute" : "relative", ifnbuff);
+
+	// TODO: 如果输入路径不是合法文件（如是个目录），或者该文件不存在/不可读，则报错退出；
+
+	// 2. 检查是否指定了输出
+	if (argc == 3) { // 没有指定，则根据输入文件名生成
+		// TODO: 其实需要先检查输入文件的扩展名的，应确保不一致，否则输出文件会将输入文件覆盖
+		cwk_path_change_extension(ifnbuff, mode == 0 ? "png" : "bin", ofnbuff, MAX_PATHx2);
+	} else {         // 指定了输出
+		// 首先要绝对化
+		flag = cwk_path_is_absolute(argv[3]);
+		if (flag)
+			cwk_path_normalize(argv[3], ofnbuff, MAX_PATHx2);
+		else {
+			getcwd(cwd, MAX_PATH);
+			cwk_path_join(cwd, argv[3], ofnbuff, MAX_PATHx2);
+		}
+
+		// 检查是不是一个目录
+		if (isDir(ofnbuff)) { // 是目录
+			// 将输入文件名拼接到目录后，然后确定并更改扩展名
+			const char *base = NULL;
+			size_t length = 0;
+			cwk_path_get_basename(ifnbuff, &base, &length);
+			cwk_path_join(ofnbuff, base, ofnbuff, MAX_PATHx2);
+
+			// TODO: 其实需要先检查输入文件的扩展名的，应确保不一致，否则输出文件会将输入文件覆盖
+			cwk_path_change_extension(ofnbuff, mode == 0 ? "png" : "bin", ofnbuff, MAX_PATHx2);
+		}
+	}
+
+	printf("output file name is %s, a %s path; processed to %s.\n", argc == 3 ? "not specified" : argv[2], flag ? "absolute" : "relative", ofnbuff);
+	// 整理出输入文件和输出文件的绝对路径 <--
+
+	infile = ifnbuff; // mode == 0 ? BIN_IN_FILE : PNG_FILE;
+	outfile = ofnbuff ; // mode == 0 ? PNG_FILE : BIN_OUT_FILE;
 
 	if (mode == 0)
 		binaryToPng(infile, outfile);
@@ -49,8 +100,7 @@ void main(int argc, char *argv[]) {
 }
 
 int printUsage(int argc, char *argv[]) {
-	printf("Usage: %s (encrypt|decrypt)", argv[0]);
-	puts("");
+	printf("Usage: %s <encrypt|decrypt> input [output]\n", argv[0]);
 }
 
 // Convert a binary file to PNG
@@ -111,7 +161,7 @@ int binaryToPng(char *infile, char *outfile) {
 	for (i = binarySize; i < pngDataSize - 1; i++)
 		pngData[i] = 0;
 
-	puts("Starting conversion to PNG file.");
+	printf("Starting conversion to PNG file.\n");
 
 	printf("Writing PNG file to %s...\n", outfile);
 
@@ -125,7 +175,7 @@ int binaryToPng(char *infile, char *outfile) {
 		printf("error %u: %s.\n", error, lodepng_error_text(error));
 		return -1;
 	} else {
-		puts("Success!");
+		printf("Success!\n");
 		return 0;
 	}
 }
@@ -140,7 +190,7 @@ int pngToBinary(char *infile, char *outfile) {
 
 	int imageWidth, imageHeight;
 
-	printf("Reading PNG file (%s)...\n", PNG_FILE);
+	printf("Reading PNG file (%s)...\n", infile);
 
 	// Decode PNG file to pngData array
 	error = lodepng_decode32_file(&pngData, &imageWidth, &imageHeight, infile);
@@ -154,9 +204,9 @@ int pngToBinary(char *infile, char *outfile) {
 
 	printf("Size of input image: %d x %d px.\n", imageWidth, imageHeight);
 
-	puts("Starting conversion to binary file.");
+	printf("Starting conversion to binary file.\n");
 
-	printf("Writing binary file to %s...\n", BIN_OUT_FILE);
+	printf("Writing binary file to %s...\n", outfile);
 
 	// Write data to binary file
 	binaryFile = fopen(outfile, "wb");
@@ -169,12 +219,18 @@ int pngToBinary(char *infile, char *outfile) {
 	} else {
 		fwrite(binaryBuff, fileSize, 1, binaryFile);
 
-		puts("Success!");
+		printf("Success!\n");
 
 		free(pngData);
 		fclose(binaryFile);
 
 		return 0;
 	}
+}
+
+bool isDir(const char *path) {
+	struct stat buf;
+	stat(path, &buf);
+	return S_ISDIR(buf.st_mode);
 }
 
